6d5512cb0ec3827022c635ee3a4464b272d0974f
Receiver1 <receiver8@yandex.ru>
2021-08-15 10:58:55 +0300

    vector fixes

--- sdk/vector.hpp
+++ sdk/vector.hpp
@@ -1,11 +1,8 @@
 #pragma once
 
-//#include <cmath>
-#include <math.h>
+#include <cmath>
 
-struct s_vector2
-{
-public:
+struct s_vector2 {
 	float x, y;
 };
 
@@ -15,7 +12,7 @@ struct s_vector3
 
 	s_vector3() : x(0.0f), y(0.0f), z(0.0f) {};
 	s_vector3(float x, float y, float z) : x(x), y(y), z(z) {};
-	//s_vector3(c_navigation right) : x(right.x), y(right.y), z(right.z) {};
+	s_vector3(c_navigation right) : x(right.position.x), y(right.position.y), z(right.position.z) {};
 
 	s_vector3 operator-(s_vector3 right)
 	{
@@ -42,6 +39,6 @@ struct s_vector3
 	float distance(s_vector3 right)
 	{
 		s_vector3 d = right - *this;
-		return sqrtf(d.x * d.x + d.y * d.y + d.z * d.z);
+		return std::sqrtf(d.x * d.x + d.y * d.y + d.z * d.z);
 	}
 };
